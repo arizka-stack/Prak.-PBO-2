aed5e1896b0b5c40e36fba19cf930a1b544f89ce
Aukhal Rifqi Zahrandhika <aukhalrifqizahrandhika@outlook.com>
2024-09-01 11:37:32 +0700

    first commit

---
+++ Aukhal_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aukhal_project1;
+
+/**
+ *
+ * @author Aukhal Rifqi Z
+ */
+public class Aukhal_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Aukhal Rifqi Z");
+        System.out.println("NPM: NPM_kalian");
+        System.out.println("Alamat: Alamat_kalian");
+        System.out.println("No_HP: No_HP_kalian");
+    }
+}
